4e8916efba2c3393b0ed35c8980c1296e0008c0a
sharifmrahat <sharifmrahat@gmail.com>
2023-04-02 16:38:39 +0600

    users service api is updated

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,50 @@
+import { GetUserArgs, GetUsersArgs } from './dto/user.args.dto';
+import {
+  CreateUserInput,
+  UpdateUserInput,
+  DeleteUserInput,
+} from './dto/user.input.dto';
 import { User } from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
+  private repository: User[] = [];
 
-  public createUser(): User {}
+  public createUser(createUserData: CreateUserInput): User {
+    const user: User = {
+      userId: uuid(),
+      ...createUserData,
+    };
+    this.repository.push(user);
+    return user;
+  }
 
-  public updateUser(): User {}
+  public updateUser(updateUserData: UpdateUserInput): User {
+    const user = this.repository.find(
+      (user) => user.userId === updateUserData.userId,
+    );
 
-  public getUser(): User {}
+    Object.assign(user, updateUserData);
 
-  public getUsers(): [User] {}
+    return user;
+  }
 
-  public deleteUser(): User {}
+  public getUser(getUserArgs: GetUserArgs): User {
+    return this.repository.find((user) => user.userId === getUserArgs.userId);
+  }
+
+  public getUsers(getUsersArgs: GetUsersArgs): User[] {
+    return getUsersArgs.userIds.map((userId) => this.getUser({ userId }));
+  }
+
+  public deleteUser(deleteUserData: DeleteUserInput): User {
+    const userIndex = this.repository.findIndex(
+      (user) => user.userId === deleteUserData.userId,
+    );
+    const deletedUser = this.repository[userIndex];
+    this.repository.splice(userIndex);
+    return deletedUser;
+  }
 }
