11066bbe0a4c48895b42c7ac4521af8d8cbb7221
sharifmrahat <sharifmrahat@gmail.com>
2023-04-09 17:45:42 +0600

    host url is udpated

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 8080;
-  const hostUrl = process.env.HOST_URL || 'http://localhost';
+  const hostUrl = process.env.HOST_URL || `http://localhost:${port}`;
   app.listen(port, () =>
-    console.log(`✅ Success! Server started on ${hostUrl}:${port}/graphql 🚀`),
+    console.log(`✅ Success! Server started on ${hostUrl}/graphql 🚀`),
   );
 }
 bootstrap();
